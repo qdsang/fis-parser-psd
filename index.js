6733de9ffde269dcca51f46c6100b1843bf4537f
fenglong <long.feng@niu.com>
2015-12-06 12:13:16 +0800

    add build cache

--- index.js
+++ index.js
@@ -5,8 +5,17 @@
 var psd2png = require('psd2png');
 
 module.exports = function(content, file, conf){
+    var hash = fis.util.md5(content, 10);
+    var basename = fis.project.getCachePath('psd', file.basename);
+    var cacheFile = basename + '-psd-' + hash + '.tmp';
     
-    var buffer = psd2png(content);
-
+    var buffer;
+    if (fis.util.exists(cacheFile)) {
+    	buffer = fis.util.fs.readFileSync(cacheFile);
+    } else {
+    	buffer = psd2png(content);
+     	fis.util.write(cacheFile, buffer);
+    }
+    
     return buffer;
 };
